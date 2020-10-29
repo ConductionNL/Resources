d024c747d97e34c2c957cffb77069c2a79a70af5
Wilco Louwerse <wilco@conduction.nl>
2020-10-29 12:30:52 +0100

    Organization apifilter in application & added contact in zuid-drecht organization datafixtures

--- api/src/Entity/Application.php
+++ api/src/Entity/Application.php
@@ -65,7 +65,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiFilter(BooleanFilter::class)
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
- * @ApiFilter(SearchFilter::class)
+ * @ApiFilter(SearchFilter::class, properties={"organization": "partial"})
  */
 class Application
 {
