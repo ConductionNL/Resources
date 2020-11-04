5ad9e2a8aa7c5e7f980a09dec9be3418dc4ac708
Barry Brands <barrybrands02@hotmail.com>
2020-11-04 15:44:25 +0100

    CGFixtures added

---
+++ api/src/DataFixtures/CommongroundFixtures.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Application;
+use App\Entity\Configuration;
+use App\Entity\Image;
+use App\Entity\Menu;
+use App\Entity\MenuItem;
+use App\Entity\Organization;
+use App\Entity\Slug;
+use App\Entity\Style;
+use App\Entity\Template;
+use App\Entity\TemplateGroup;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class CommongroundFixtures extends Fixture
+{
+    private $params;
+    /**
+     * @var CommonGroundService
+     */
+    private $commonGroundService;
+
+    public const ORGANIZATION_CONDUCTION = 'organization-conduction';
+
+    public function __construct(ParameterBagInterface $params, CommonGroundService $commonGroundService)
+    {
+        $this->params = $params;
+        $this->commonGroundService = $commonGroundService;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // Lets make sure we only run these fixtures on larping enviroment
+        if (
+            !$this->params->get('app_build_all_fixtures') &&
+            $this->params->get('app_domain') != 'commonground.nu' && strpos($this->params->get('app_domain'), 'commonground.nu') == false) {
+            return false;
+        }
+        //var_dump($this->params->get('app_domain'));
+
+        // Deze organisaties worden ook buiten het wrc gebruikt
+        $id = Uuid::fromString('073741b3-f756-4767-aa5d-240f167ca89d');
+        $conduction = new Organization();
+        $conduction->setName('Conduction');
+        $conduction->setDescription('Conduction');
+        $conduction->setRsin('');
+        $conduction->setContact($this->commonGroundService->cleanUrl(['component'=>'cc', 'type'=>'organizations', 'id'=>'9650a44d-d7d1-454a-ab4f-2338c90e8c2f']));
+        $manager->persist($conduction);
+        $conduction->setId($id);
+        $manager->persist($conduction);
+        $manager->flush();
+        $conduction = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+
+        // Website applicatie
+        $id = Uuid::fromString('7d19fbc6-6c35-4087-ab10-9778277cefe1');
+        $website = new Application();
+        $website->setName('Commonground.nu');
+        $website->setDescription('Commonground.nu applicatie');
+        $website->setDomain('commonground.nu');
+        $website->setOrganization($conduction);
+        $manager->persist($website);
+        $website->setId($id);
+        $manager->persist($website);
+        $manager->flush();
+        $website = $manager->getRepository('App:Application')->findOneBy(['id'=> $id]);
+    }
+}
